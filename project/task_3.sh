719e9a9f664cc5e31752f417318504c0210115d9
Adam Dłubak <adlubak@pgs-soft.com>
2018-10-16 15:34:06 +0200

    Task_3 fixed / refactored

--- project/task_3.sh
+++ project/task_3.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-#Ściezki bezwzgledne
-
-
 if [ "$#" != "1" ]
 then
 	echo "Niepoprawna lista parametrow!"
 	exit 1;
 fi
 
-
-IFS=$'\n' read -d '' -r -a lines < $1
-
+# Version 1
 echo 'Version 1:':	
-
-# echo "$RANDOM % 6" | bc
 random_line=$(echo "$RANDOM % 6" | bc)
 head -n $random_line $1 | tail -n 1
 
-
+# Version 2
 echo 'Version 2:':
 shuf -n 1 $1
